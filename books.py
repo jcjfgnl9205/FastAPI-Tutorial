c895d5150f35a2e0601079eb449152704fc65efa
jcjfgnl9205 <jcjfgnl9205@gmail.com>
2021-12-31 14:41:47 +0900

    Post Request

--- books.py
+++ books.py
@@ -55,3 +55,21 @@ async def get_direction(direction_name : DirectionName):
 @app.get("/{book_name}")
 async def read_book(book_name: str):
     return BOOKS[book_name]
+
+
+@app.post("/")
+async def create_book(book_title, book_author):
+    current_book_id = 0
+    
+    if len(BOOKS) > 0:
+        for book in BOOKS:
+            x = int(book.split('_')[-1])
+            if x > current_book_id:
+                current_book_id = x
+                
+    BOOKS['book_{id}'.format(id=current_book_id + 1)] = {
+        'title': book_title,
+        'author': book_author
+    }
+    
+    return BOOKS['book_{id}'.format(id=current_book_id + 1)]
