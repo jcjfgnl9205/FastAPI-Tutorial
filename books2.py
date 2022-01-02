fb6458d731002f8ff63efe8c5a2e1b74026460e8
jcjfgnl9205 <jcjfgnl9205@gmail.com>
2022-01-02 10:35:57 +0900

    CRUD with Book class

---
+++ books2.py
@@ -0,0 +1,103 @@
+from typing import Optional
+
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+from uuid import UUID
+
+
+app = FastAPI()
+
+
+class Book(BaseModel):
+    id: UUID
+    title: str = Field(min_length=1)
+    author: str = Field(min_length=1, max_length=100)
+    description: Optional[str] = Field(title="Description of the book",
+                                       max_length=100,
+                                       min_length=1)
+    rating: int = Field(gt=-1, lt=101)
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "id": "6c877ec8-821e-4ef2-bdfa-443f42120ad2",
+                "title": "TITLE",
+                "author": "AUTHOR",
+                "description": "DESCRIPTION",
+                "rating": 75
+            }
+        }
+
+
+BOOKS = []
+
+
+@app.get("/")
+async def read_all_books(books_to_return: Optional[int] = None):
+    if len(BOOKS) < 1:
+        create_books_no_api()
+
+    if books_to_return and len(BOOKS) >= books_to_return > 0:
+        i = 1
+        new_books = []
+        while i <= books_to_return:
+            new_books.append(BOOKS[i - 1])
+            i += 1
+        return new_books
+    return BOOKS
+
+
+@app.get("/book/{book_id}")
+async def read_book(book_id: UUID):
+    for book in BOOKS:
+        if book.id == book_id:
+            return book
+
+
+@app.post("/")
+async def create_book(book: Book):
+    BOOKS.append(book)
+    return book
+
+
+@app.put("/{book_id}")
+async def update_book(book_id: UUID, book:Book):
+    counter = 0
+    
+    for x in BOOKS:
+        counter += 1
+        if x.id == book_id:
+            BOOKS[counter - 1] = book
+            return BOOKS[counter - 1]
+
+
+@app.delete("/{book_id}")
+async def delete_book(book_id: UUID):
+    counter = 0
+    
+    for x in BOOKS:
+        counter += 1
+        if x.id == book_id:
+            del BOOKS[counter - 1]
+            return '{book_id} が削除されました'.format(book_id=book_id)
+
+
+def create_books_no_api():
+    book_1 = Book(id="6c877ec8-821e-4ef2-bdfa-443f42120ad2",
+                  title="Title 1",
+                  author="Author 1",
+                  description="Description 1",
+                  rating=60)
+    book_2 = Book(id="7c877ec8-821e-4ef2-bdfa-443f42120ad2",
+                  title="Title 2",
+                  author="Author 2",
+                  description="Description 2",
+                  rating=70)
+    book_3 = Book(id="8c877ec8-821e-4ef2-bdfa-443f42120ad2",
+                  title="Title 3",
+                  author="Author 3",
+                  description="Description 3",
+                  rating=80)
+    BOOKS.append(book_1)
+    BOOKS.append(book_2)
+    BOOKS.append(book_3)
